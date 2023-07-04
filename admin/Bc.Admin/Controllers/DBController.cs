f3bb21a9b9aa4f96dbd8a6d17c6f13c117a3d6d9
602392714@qq.com <602392714@qq.com>
2023-07-04 08:21:23 +0800

    git

---
+++ admin/Bc.Admin/Controllers/DBController.cs
@@ -0,0 +1,71 @@
+﻿using Bc.Admin.Infrastructures;
+using Bc.Bussiness.IService;
+using Bc.Bussiness.Service;
+using Bc.Common.Helpers;
+using Bc.Model;
+using Bc.Model.Dto;
+using Bc.Model.Dto.System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bc.Admin.Controllers
+{
+    [WebAuthorizeAttribute]
+    public class DBController : BaseController
+    {
+        private readonly IDbConfigService biz;
+        private readonly IGroupTableService bizGT;
+
+
+        public DBController()
+        {
+            biz = new DbConfigService();
+            bizGT = new GroupTableService();
+        }
+        public IActionResult config()
+        {
+            return View();
+        }
+        public IActionResult mydb()
+        {
+            return View();
+        }
+        public IActionResult group()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetList(DbConfigDto search)
+        {
+            var lst = biz.QueryPages(search);
+            return toResponse(lst.TotalCount, "", lst);
+        }
+        [HttpGet]
+        public ActionResult GroupTableList(GroupTableDto search)
+        {
+            var lst = bizGT.QueryPages(search);
+            return toResponse(lst.TotalCount, "", lst);
+        }
+
+
+        #region 表信息
+        [HttpGet]
+        public ActionResult GetTableColumn(string configno, string database, string table)
+        {
+            var lst = biz.GetTableColumn(configno, database, table);
+            return toResponse(1, "", lst);
+        }
+        [HttpGet]
+        public ActionResult GetTableRows(string configno, string database = "", string tablename = "")
+        {
+            var lst = biz.GetTableRows(configno, database, tablename);
+            return toResponse(1, "", JsonHelper.ToJson(lst));
+        }
+        [HttpGet]
+        public ActionResult GetDatabaseList(string configno)
+        {
+            var lst = biz.GetDatabaseList(configno);
+            return toResponse(1, "", lst);
+        }
+        #endregion
+    }
+}
---
+++ admin/Bc.Model/View/TableInfo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bc.Model.View
+{
+    /// <summary>
+    /// 表信息
+    /// </summary>
+    public class TableInfo
+    {
+        
+        public int Id { get; set; }
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// 表说明
+        /// </summary>
+        public string TableNameDesc { get; set; }
+
+        /// <summary>
+        /// 字段序号
+        /// </summary>
+        public int ColSort { get; set; }
+
+        /// <summary>
+        /// 自增标识
+        /// </summary>
+        public string Identity { get; set; }
+
+        /// <summary>
+        /// 字段名
+        /// </summary>
+        public string Column { get; set; }
+
+        /// <summary>
+        /// 标识
+        /// </summary>
+        public string PrimaryKey { get; set; }
+
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// 占用字节数
+        /// </summary>
+        public int ByteLen { get; set; }
+
+
+
+        /// <summary>
+        /// 长度
+        /// </summary>
+        public int Len { get; set; }
+
+        /// <summary>
+        /// 小数位
+        /// </summary>
+        public int Digtal { get; set; }
+
+        /// <summary>
+        /// 允许为空
+        /// </summary>
+        public string AllowNull { get; set; }
+
+        /// <summary>
+        /// 默认值
+        /// </summary>
+        public string DefaultValue { get; set; }
+
+        /// <summary>
+        /// 列描述
+        /// </summary>
+        public string ColumnDesc { get; set; }
+
+
+        /// <summary>
+        /// 数据库
+        /// </summary>
+        public string Database { get; set; }
+
+        /// <summary>
+        /// 别名
+        /// </summary>
+
+        public string Alias { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+    }
+
+}
